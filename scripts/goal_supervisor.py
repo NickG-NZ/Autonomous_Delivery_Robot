4338c3fe54691ed3aa320caec9dcf6d85b3dc69d
Jason Anderson <janderson271@berkeley.edu>
2019-11-30 20:02:17 -0800

    untested goal supervisor...compiles and runs in roscore

---
+++ scripts/goal_supervisor.py
@@ -0,0 +1,118 @@
+
+import rospy
+from geometry_msgs.msg import Pose2D, PoseStamped
+from std_msgs.msg import Bool
+
+class GoalSupervisor:
+	def __init__(self):
+
+		# state machine space
+		self.vending = False
+		self.laser_pointer_control = False
+		self.rviz_control = False
+
+		# update flag for self.main_loop()
+		self.publish_cmd = False
+
+		rospy.init_node('Goal_Supervisor')
+		"""
+		ROS Subscribers
+		"""
+		self.vending_cmd_cache = None
+		self.vending_subscriber = \
+		rospy.Subscriber('/vending_cmd', PoseStamped, self.vending_callback)
+
+		# Goal Topic Subscriptions
+		self.laser_pointer_cmd_cache = None
+		self.laser_pointer_subscriber = \
+		rospy.Subscriber('/laser_pointer_cmd', PoseStamped, self.laser_pointer_callback)
+
+		self.rviz_cmd_cache = None
+		self.rviz_subscriber =  \
+		rospy.Subscriber('/rviz_cmd', PoseStamped, self.rviz_callback)
+
+		# State Machine Subscriptions
+		rospy.Subscriber('/reached_goal', Bool, self.reached_goal_callback)
+		rospy.Subscriber('/resume_vending', Bool, self.resume_vending_callback)
+
+		"""
+		ROS Publishers
+		"""
+		self.cmd_nav_publisher = rospy.Publisher('/cmd_nav', Pose2D, queue_size=1)
+		self.request_vending_cmd = rospy.Publisher('/request_vending_cmd', Bool, queue_size=1)
+		self.request_vending_replan = rospy.Publisher('request_vending_replan', Bool, queue_size=1)
+
+	def main_loop(self):
+
+		if self.publish_cmd:
+			if self.rviz_control:
+				self.publish(self.rviz_cmd_cache)
+			elif self.laser_pointer_control:
+				self.publish(self.laser_pointer_control)
+			elif self.vending:
+				self.publish(self.vending_cmd_cache)
+
+			self.publish_cmd = False
+		
+
+	def idle(self):
+		self.vending = False
+		self.laser_pointer_control = False
+		self.rviz_control = False
+
+	def laser_pointer_callback(self, msg):
+		# laser_pointer has priority over vending but no priority over rviz
+		if not self.rviz_control:
+			self.laser_pointer_control = True
+			self.vending = False
+			self.rviz_control = False
+			self.publish_cmd = True
+
+		self.laser_pointer_cmd_cache = msg.pose
+		self.laser_pointer_cmd_time = msg.header.stamp
+
+	def vending_callback(self, msg):
+		self.laser_pointer_cmd_cache = msg.pose
+
+		if self.vending:
+			self.publish_cmd = True
+
+	def rviz_callback(self, msg):
+		# rviz has first priority over laser_pointer_control and vending
+		self.vending = False
+		self.laser_pointer_control = False
+		self.rviz_control = True
+
+		self.rviz_cmd_cache = msg.pose
+		self.publish_cmd = True
+
+	def reached_goal_callback(self, msg):
+
+		if self.rviz_control:
+			self.idle()
+		elif self.laser_pointer_control:
+			self.idle()
+		elif self.vending():
+			msg = Bool()
+			self.request_vending_cmd(msg)
+
+	def resume_vending_callback(self, msg):
+		self.vending = True
+		self.laser_pointer_control = False
+		self.rviz_control = False
+		self.publish_cmd = True
+
+		msg = Bool()
+		self.request_vending_replan(msg)
+
+	def publish_cmd_nav(self, msg):
+		self.cmd_nav_publisher.publish(msg)
+
+	def run(self):
+		self.main_loop()
+		rospy.spin()
+
+
+if __name__ == '__main__':
+	gs = GoalSupervisor()
+	gs.run()
