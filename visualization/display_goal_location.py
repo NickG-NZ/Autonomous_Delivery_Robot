bc8c14a1e8fc02b8bfeb3a2435a44585d11cb726
Jing Gu <jing.gu@tafsm.org>
2019-12-01 00:18:33 -0800

    add files to record and visualize food locations

--- visualization/display_goal_location.py
+++ visualization/display_goal_location.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
+
 import rospy
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Pose2D, PoseStamped
+import tf
+
 X = 0
 Y = 0
 Z = 0
+THETA = 0
 
 def callback(msg):
     global X, Y
     X = msg.x
     Y = msg.y
-    print('goal update {0}, {1}'.format(X, Y))
+    THETA = msg.theta
+    # print('goal update {0}, {1}, {2}'.format(X, Y, THETA))
     return
 
 def publisher():
-    vis_pub = rospy.Publisher('marker_topic', Marker, queue_size=10)
-    rospy.init_node('marker_node', anonymous=True)
+    vis_pub = rospy.Publisher('/goal_marker', Marker, queue_size=10)
+    rospy.init_node('goal_marker', anonymous=True)
     rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
@@ -31,28 +36,29 @@ def publisher():
         #            each need to have a separate marker ID.
         marker.id = 0
 
-        marker.type = 2 # sphere
+        marker.type = 0 # arrow
 
         marker.pose.position.x = X
         marker.pose.position.y = Y
         marker.pose.position.z = 0
 
-        marker.pose.orientation.x = 0.0
-        marker.pose.orientation.y = 0.0
-        marker.pose.orientation.z = 0.0
-        marker.pose.orientation.w = 1.0
+        q = tf.transformations.quaternion_about_axis(THETA, (0, 0, 1))
+        marker.pose.orientation.x = q[0]
+        marker.pose.orientation.y = q[1]
+        marker.pose.orientation.z = q[2]
+        marker.pose.orientation.w = q[3]
 
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
+        marker.scale.x = 0.2
+        marker.scale.y = 0.05
+        marker.scale.z = 0.05
 
-        marker.color.a = 0.5 # Don't forget to set the alpha!
+        marker.color.a = 1.0 # Don't forget to set the alpha!
         marker.color.r = 1.0
         marker.color.g = 0.0
         marker.color.b = 0.0
         
         vis_pub.publish(marker)
-        print('Published marker at {0},{1}!'.format(X, Y))
+        # print('Published marker at {0},{1}!'.format(X, Y))
         
         rate.sleep()
 
