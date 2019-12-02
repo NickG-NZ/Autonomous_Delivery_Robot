4471ffa975fbd26caf8e5d0395e48d60421069c6
Jing Gu <jing.gu@tafsm.org>
2019-12-01 22:52:15 -0800

    save progress on vending node

--- scripts/vending_planner.py
+++ scripts/vending_planner.py
@@ -7,6 +7,7 @@ from asl_turtlebot.msg import FoodMap
 from std_msgs.msg import String, Bool
 import numpy as np
 from grids import StochOccupancyGrid2D
+from planners import AStar
 
 
 class Vending_Planner:
@@ -15,7 +16,7 @@ class Vending_Planner:
         # waypoint queue, list of np arrays
         self.queue = []
         # threshold on collision probability when finding waypoint corresponding to food
-        self.occupancy_thres = 0.05
+        self.occupancy_thres = 0.1
         # map parameters
         self.map_width = 0
         self.map_height = 0
@@ -69,7 +70,7 @@ class Vending_Planner:
                                                   self.map_origin[0],
                                                   self.map_origin[1],
                                                   8,
-                                                  self.map_probs)
+                                                  self.map_probs, self.occupancy_thres)
             if self.foodmap:
                 # find waypoints for each food
                 self.find_waypoints()
@@ -90,9 +91,64 @@ class Vending_Planner:
             self.build_connected_graph()
 
     def find_waypoints(self):
+        """
+        find waypoint (points that are close to food and is collision free) corresponding to each food
+        :return: None. Updates self.food_waypoint
+        """
+        # get all the grid points in the map
+        x_coords = self.occupancy.resolution * np.array(range(self.occupancy.width)) \
+                   + self.occupancy.origin_x * np.ones(self.occupancy.width)
+        y_coords = self.occupancy.resolution * np.array(range(self.occupancy.height)) \
+                   + self.occupancy.origin_y * np.ones(self.occupancy.height)
+        x_coords, y_coords = np.meshgrid(x_coords, y_coords)
+        x_coords_flat = np.ndarray.flatten(x_coords)
+        y_coords_flat = np.ndarray.flatten(y_coords)
+        # sort the grid points based on distances from food
+        for obj, coord in self.foodmap.items():
+            dist = np.sqrt((x_coords - coord[0] * np.ones(x_coords.shape))**2
+                           + (y_coords - coord[1] * np.ones(y_coords.shape))**2)
+            inds = np.argsort(np.ndarray.flatten(dist))
+            waypoint = np.array([self.occupancy.origin_x, self.occupancy.origin_y])
+            # find the closest point that is collision free
+            for ind in inds:
+                waypoint = np.array([x_coords_flat[ind], y_coords_flat[ind]])
+                if self.occupancy.is_free(waypoint):
+                    break
+            self.food_waypoint[obj] = waypoint
         return
 
     def build_connected_graph(self):
+        """
+        calculate the distance between each food object
+        :return: None. Update self.distance_mat, such that element i, j is the distance from food i to food j,
+        where i and j are the index in self.food_list
+        """
+        # create list of food so index of each food is fixed
+        self.food_list = self.food_waypoint.keys()
+        nfood = len(self.food_list)
+        # bounds for Astar search
+        state_min = np.array([self.occupancy.origin_x, self.occupancy.origin_y])
+        state_max = np.array([self.occupancy.origin_x + self.occupancy.resolution * self.occupancy.width,
+                              self.occupancy.origin_y + self.occupancy.resolution * self.occupancy.height])
+        # init distance_mat
+        self.distance_mat = np.full([nfood, nfood], np.inf)
+        for i in range(nfood):
+            foodi = self.food_list[i]
+            coordi = self.food_waypoint[foodi]
+            # distance to self is zero
+            self.distance_mat[i, i] = 0
+            for j in range(i):
+                foodj = self.food_list[j]
+                coordj = self.food_waypoint[foodj]
+                # solve Astar
+                problem = AStar(state_min, state_max, coordi, coordj, self.occupancy, self.occupancy.resolution)
+                success = problem.solve()
+                if success:
+                    # get total distance
+                    cost = problem.est_cost_through[problem.path[-2]]
+                    # assume symmetry, set both i to j and j to i to distance calculated
+                    self.distance_mat[i, j] = cost
+                    self.distance_mat[j, i] = cost
         return
 
     def request_callback(self, msg):
@@ -106,4 +162,13 @@ class Vending_Planner:
         return
 
     def cmd_callback(self):
+        # remove first waypoint from queue and publish it
+        coord = self.queue.pop(0)
+        waypoint = Pose2D()
+        waypoint.x = coord[0]
+        waypoint.y = coord[1]
+        waypoint.theta = 0
+        self.waypoint_pub.publish(waypoint)
 
+    def replan_callback(self):
+        self.replan()
